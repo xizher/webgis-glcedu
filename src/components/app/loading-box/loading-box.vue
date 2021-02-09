77786517306a668d708e34c6350ee667f189d15e
xizher <xizher@163.com>
2021-02-09 18:18:53 +0800

    feat: 加载组件样式调整

--- src/components/app/loading-box/loading-box.vue
+++ src/components/app/loading-box/loading-box.vue
@@ -35,6 +35,17 @@ export default {
   display: flex;
   justify-content: center;
   align-items: center;
+
+  &::after {
+    content: '';
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-color: #fff;
+    opacity: .3;
+  }
 }
 .loader {
   display: inline-block;
