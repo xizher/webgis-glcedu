c27c34d7a503b57f9b95fc8ae2082d1e360e9fb0
xizher <xizher@163.com>
2021-03-31 20:33:20 +0800

    备份

--- src/config/app.config.js
+++ src/config/app.config.js
@@ -1,5 +1,5 @@
-const ARCGIS_JS_API = 'http://localhost/arcgis/api/4.17'
-const ARCGIS_SERVER_URL = 'http://192.168.65.130:6080/arcgis'
+const ARCGIS_JS_API = 'http://192.168.0.105/arcgis/api/4.17'
+const ARCGIS_SERVER_URL = 'http://192.168.65.134:6080/arcgis'
 
 export default {
   apiConfig: {
